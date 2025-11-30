53e99cb8f1c75c773106c70d4d3ae05ff04d7d8c
corb0140 <corb0140@algonquinlive.com>
2025-11-30 14:33:02 -0500

    updated projects.json, design changes to project-page

--- src/pages/project-page.jsx
+++ src/pages/project-page.jsx
@@ -4,7 +4,7 @@ import { SquareArrowOutUpRight } from "lucide-react";
 
 function ProjectPage() {
   const { projectId } = useParams();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     try {
@@ -26,18 +26,36 @@ function ProjectPage() {
   if (!data) return <p>Loading...</p>;
 
   return (
-    <div className="h-auto p-5 flex flex-col gap-5">
-      <div className="mt-10 mb-20">
+    <div className="h-auto flex flex-col gap-5 overflow-hidden">
+      <div className="mt-10 mb-20 p-5">
         <h1 className="uppercase font-bold text-4xl max-w-100 tracking-wide">
           {data.title}
         </h1>
       </div>
 
-      <div className="h-80 w-full border rounded-md">
-        <img src={data.image} alt={data.title + " image"} />
+      <div className="h-70 w-full rounded-md pl-5 overflow-x-auto no-scrollbar">
+        <div
+          className="relative h-full"
+          style={{
+            width: `${data.demos.length * 100 - 20}%`,
+          }}
+        >
+          {data.demos.map((item, index) => (
+            <div
+              key={index}
+              className="absolute w-75 h-full border rounded-md"
+              style={{
+                transform: `translateX(${index * 100}%)`,
+                marginLeft: `${index * 0.7}rem`,
+              }}
+            >
+              {item.demo}
+            </div>
+          ))}
+        </div>
       </div>
 
-      <div className="flex gap-1.5 flex-wrap">
+      <div className="flex gap-1.5 flex-wrap p-5">
         {data.tech?.map((item, index) => (
           <span
             key={item + index}
@@ -48,28 +66,34 @@ function ProjectPage() {
         ))}
       </div>
 
-      <p>{data.description}</p>
+      <p className="p-5">{data.description}</p>
 
-      <div className="flex gap-3">
-        <a
-          href={data.external_links?.demo}
-          target="_blank"
-          className="bg-primary/90 py-2.5 px-4.5 text-sm flex items-center gap-1 text-tertiary rounded-sm"
-        >
-          Demo
-          <SquareArrowOutUpRight className="h-4 w-4" />
-        </a>
-        <a
-          href={data.external_links?.github}
-          target="_blank"
-          className="bg-primary/90 py-2.5 px-4.5 text-sm flex items-center gap-1 text-tertiary rounded-sm"
-        >
-          Github
-          <SquareArrowOutUpRight className="h-4 w-4" />
-        </a>
+      {/* EXTERNAL LINKS */}
+      <div className="p-5 flex flex-col gap-3">
+        <div className="flex gap-3">
+          <a
+            href={data.external_links.demo}
+            target="_blank"
+            className="bg-primary/90 py-2.5 px-4.5 text-sm flex items-center gap-1 text-tertiary rounded-sm"
+          >
+            Demo
+            <SquareArrowOutUpRight className="h-4 w-4" />
+          </a>
+
+          <a
+            href={data.external_links.github}
+            target="_blank"
+            className="bg-primary/90 py-2.5 px-4.5 text-sm flex items-center gap-1 text-tertiary rounded-sm"
+          >
+            Github
+            <SquareArrowOutUpRight className="h-4 w-4" />
+          </a>
+        </div>
+
+        {data.notes && <p className="text-sm">Note: {data.notes}</p>}
       </div>
 
-      <div className="mt-10 flex flex-col gap-10">
+      <div className="mt-10 flex flex-col gap-10 p-5">
         <h2 className="text-primary/90 uppercase text-[17px]">
           Why This Project
         </h2>
